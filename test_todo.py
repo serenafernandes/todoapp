401db46dd74f44d03ad8d2b21d3027b55b5f477f
serenafernandes <serenafribeiro@gmail.com>
2018-04-14 15:57:08 -0300

    Novos testes

--- test_todo.py
+++ test_todo.py
@@ -1,4 +1,4 @@
-from todo import app
+from todo import app, tarefas
 
 
 def test_listar_tarefas_deve_retornar_status_200():
@@ -15,3 +15,14 @@ def test_lista_de_tarefas_vazia_retorna_lista_vazia():
     with app.test_client() as cliente:
         resposta = cliente.get('/task')
         assert resposta.data == b'[]\n'
+
+def test_lista_de_tarefas_nao_vazia_retorna_conteudo():
+    tarefas.append({'id': 1, 'titulo': 'tarefa 1',
+                    'descricao': 'tarefa de numero 1', 'estado': False})
+    with app.test_client() as client:
+        resposta = client.get('/task')
+        assert resposta.data == (b'[\n  {\n    "descricao": '
+                                 b'"tarefa de numero 1", \n    '
+                                 b'"estado": false, \n    '
+                                 b'"id": 1, \n    '
+                                 b'"titulo": "tarefa 1"\n  }\n]\n')
