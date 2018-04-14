819b0a47ada9d1d380576eb79ec0df6463a54b7c
serenafernandes <serenafribeiro@gmail.com>
2018-04-14 14:35:28 -0300

    Adding dependencies

--- test_todo.py
+++ test_todo.py
@@ -1,4 +1,17 @@
+from todo import app
+
+
 def test_listar_tarefas_deve_retornar_status_200():
     with app.test_client() as cliente:
         resposta = cliente.get('/task')
         assert resposta.status_code == 200
+
+def test_listar_tarefas_deve_ter_formato_json():
+    with app.test_client() as cliente:
+        resposta = cliente.get('/task')
+        assert resposta.content_type == 'application/json'
+
+def test_lista_de_tarefas_vazia_retorna_lista_vazia():
+    with app.test_client() as cliente:
+        resposta = cliente.get('/task')
+        assert resposta.data == b'[]\n'
--- todo.py
+++ todo.py
@@ -1,3 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 
 app = Flask('todoapp')
+
+@app.route('/task')
+def listar():
+    return jsonify()
